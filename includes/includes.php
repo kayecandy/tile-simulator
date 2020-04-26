876c4f1b6a7213a362ca8d005d0275673bc4c39c
Candice Canoso <candice.canoso@comgateway.com>
2020-04-26 20:06:50 +0800

    Rename artise- to tile-

--- includes/includes.php
+++ includes/includes.php
@@ -3,10 +3,10 @@ defined( 'ABSPATH' ) or die ( 'I\'m a plugin! Please don\'t access me directly!'
 
 
 
-require_once( 'artise-simulator-tile-mask/artise-simulator-tile-mask.php' );
-require_once( 'artise-simulator-tile/artise-simulator-tile.php' );
-require_once( 'artise-simulator-colors/artise-simulator-colors.php' );
-require_once( 'artise-simulator-submission/artise-simulator-submission.php' );
+require_once( 'tile-simulator-tile-mask/tile-simulator-tile-mask.php' );
+require_once( 'tile-simulator-tile/tile-simulator-tile.php' );
+require_once( 'tile-simulator-colors/tile-simulator-colors.php' );
+require_once( 'tile-simulator-submission/tile-simulator-submission.php' );
 
 
 ?>
