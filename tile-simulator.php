384d99b69c50717c814de1a5be41b85d5701bbd7
Candice Canoso <kaye_candy14@yahoo.com>
2020-06-07 19:48:44 -0700

    Update Author URI

--- tile-simulator.php
+++ tile-simulator.php
@@ -7,7 +7,7 @@ Plugin Name: Tile Simulator
 Description: Tile Simulator is a custom plugin for Lili Cement Tiles. The simulator allows users to customize tile colors, visualize available tiles in different environments and export tiles for purchase.
 Author: Candice Canoso
 Version: 2.0
-Author URI: http://www.candicecanoso.com/
+Author URI: http://www.cndce.me/
  */
 
 defined('ABSPATH') or die('I\'m a plugin! Please don\'t access me directly!');
